85a0445ff5863470082bbe1b58b7f1643f0d46d7
Messmer <prokopovaleksandr544@gmail.com>
2024-10-11 17:44:23 +0700

    fill user migration

--- database/migrations/2024_10_11_103101_create_users_table.php
+++ database/migrations/2024_10_11_103101_create_users_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->string('name')->default('Пользователь');
+            $table->string('password');
+            $table->string('email')->unique();
+            $table->string('api_token')->nullable();
             $table->timestamps();
         });
     }
