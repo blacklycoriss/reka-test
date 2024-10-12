c1d885fe04518e1d346a1cfcfa5eca6993962db7
Messmer <prokopovaleksandr544@gmail.com>
2024-10-12 09:33:37 +0700

    return all tasks

--- app/Http/Controllers/API/TaskController.php
+++ app/Http/Controllers/API/TaskController.php
@@ -4,8 +4,12 @@ namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Task;
 
 class TaskController extends Controller
 {
-    //
+    public function all()
+    {
+        return Task::all();
+    }
 }
