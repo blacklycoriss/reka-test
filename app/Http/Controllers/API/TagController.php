1716c934d43ee83e21965b3c04d493b920abe797
Messmer <prokopovaleksandr544@gmail.com>
2024-10-12 09:34:26 +0700

    return all tags

--- app/Http/Controllers/API/TagController.php
+++ app/Http/Controllers/API/TagController.php
@@ -4,8 +4,12 @@ namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Tag;
 
 class TagController extends Controller
 {
-    //
+    public function all()
+    {
+        return Tag::all();
+    }
 }
