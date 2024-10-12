cdebca868f9d5ecc6ea2eda1520a700754d770d3
Messmer <prokopovaleksandr544@gmail.com>
2024-10-12 12:58:18 +0700

    add route and layout

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\HomeController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +14,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Auth::routes();
+Route::get('/', [HomeController::class, 'index'])->name('home');
