31d7ad7db2aa41d893cab6b0806376b0b360affc
Messmer <prokopovaleksandr544@gmail.com>
2024-10-12 10:49:11 +0700

    change tag show()

--- app/Http/Controllers/API/TagController.php
+++ app/Http/Controllers/API/TagController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\TagResource;
 use App\Models\Tag;
 use Illuminate\Http\Request;
 
@@ -29,7 +30,7 @@ class TagController extends Controller
      */
     public function show(string $id)
     {
-        return Tag::find($id);
+        return TagResource::collection(Tag::find($id));
     }
 
     /**
