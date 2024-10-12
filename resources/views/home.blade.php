cc7bac51c2b33628827af0174ead6f09eacf072a
Messmer <prokopovaleksandr544@gmail.com>
2024-10-12 13:37:33 +0700

    add table and css

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,19 @@
 @extends('layouts.app')
 @section('title', 'Мои задачи')
 @section('content')
-ssss
+
+    <h2>Добро пожаловать, {{ Auth::user()->name }}!</h2>
+    <p class="text-end"><a href="">Создать задачу</a></p>
+
+{{--    @if (count($tasks) > 0)--}}
+        <table class="table table-striped table-borderless">
+            <thead>
+            <tr>
+                <th>Задача</th>
+                <th>Дата создания</th>
+                <th colspan="2">&nbsp;</th>
+            </tr>
+            </thead>
+        </table>
+{{--    @endif--}}
 @endsection('content')
